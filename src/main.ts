ce109dfdb8d2085aed6516e11d921a6ff30ddbb4
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2024-06-20 15:42:45 -0300

    :heavy_plus_sign: chore: Installing and configured packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.main/app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './core/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('WEBSERVICE');
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove properties that do not have decorators
+      transform: true, // transform payload to DTO instances
+      forbidNonWhitelisted: true, // throw error if payload has properties that do not have decorators
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  await app.listen(envs.APP_PORT);
+  logger.log(`Application is running on port: ${envs.APP_PORT}`);
 }
 bootstrap();
