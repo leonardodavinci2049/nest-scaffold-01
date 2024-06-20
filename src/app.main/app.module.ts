2c8b0e2081bde14f26dfc3f748262343a6164ef7
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2024-06-20 17:29:44 -0300

    :sparkles: feat: modules user and login

--- src/app.main/app.module.ts
+++ src/app.main/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from 'src/user/user.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
