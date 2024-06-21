aff06ca3871232929d449bff8cf3384946b7f7b9
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2024-06-21 08:10:37 -0300

    :construction: chore: starting prisma

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,41 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+  MinLength,
+} from 'class-validator';
+import { Role } from 'src/core/enums/role.enum';
+
+export class CreateUserDto {
+  @IsInt()
+  ID_SYSTEM_CFG_CLIENTE?: number;
+
+  @IsInt()
+  ID_PESSOA?: number;
+
+  @IsString({ message: 'LOGIN must be a valid string', each: true })
+  @MinLength(1)
+  LOGIN?: string;
+
+  @IsString({ message: 'NOME must be a valid string', each: true })
+  NOME?: string;
+
+  @IsOptional()
+  @IsEnum(Role)
+  ROLE?: number;
+
+  @IsEmail({}, { message: 'Invalid email' })
+  EMAIL_DE_LOGIN: string;
+
+  @IsStrongPassword({ minLength: 6 }, { message: 'Password is too weak' })
+  SENHA?: string;
+
+  @IsOptional()
+  DATADOCADASTRO?: Date;
+
+  @IsOptional()
+  DT_UPDATE?: Date;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  Put,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -16,22 +17,29 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  create(@Body() data: CreateUserDto) {
+    // console.log({ email, password });
+    return this.userService.create(data);
   }
 
   @Get()
   findAll() {
+    // return this.usersService.findAll();
     return this.userService.findAll();
   }
 
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param() id: number) {
     return this.userService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  updatePartial(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
