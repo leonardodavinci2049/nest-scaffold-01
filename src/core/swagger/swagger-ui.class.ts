d68d719e49afa2bdb3fcc9533c481f0fd634d1ea
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2024-07-10 14:26:53 -0300

    chore: add swagger

--- src/core/swagger/swagger-ui.class.ts
+++ src/core/swagger/swagger-ui.class.ts
@@ -23,6 +23,12 @@ export class SwaggerUI {
     customfavIcon: this.customfavIcon,
     customSiteTitle: this.customSiteTitle,
     customCss: this.customCss,
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
     swaggerOptions: this.swaggerOptions,
   };
 
